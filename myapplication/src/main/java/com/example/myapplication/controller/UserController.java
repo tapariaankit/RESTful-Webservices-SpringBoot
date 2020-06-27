51f0145295682dd78f54fec96463068dc0e6feea
tapariaankit <tapariaankit13@gmail.com>
2020-06-28 00:50:54 +0530

    Created model request, response and User dto

--- myapplication/src/main/java/com/example/myapplication/controller/UserController.java
+++ myapplication/src/main/java/com/example/myapplication/controller/UserController.java
@@ -4,16 +4,20 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myapplication.model.request.UserDetailsRequestModel;
+import com.example.myapplication.model.response.UserDetailsResponseModel;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 	
 	@PostMapping
-	public String createUser() {
-		return "user created";
+	public UserDetailsResponseModel createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return null;
 		
 	}
 	
