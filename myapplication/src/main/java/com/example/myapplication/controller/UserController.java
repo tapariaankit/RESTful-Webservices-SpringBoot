b02c46ebb632c558c60c5ceda1c0075fe710e44d
tapariaankit <tapariaankit13@gmail.com>
2020-06-28 00:02:52 +0530

    Create User Controller

--- myapplication/src/main/java/com/example/myapplication/controller/UserController.java
+++ myapplication/src/main/java/com/example/myapplication/controller/UserController.java
@@ -1,5 +1,35 @@
 package com.example.myapplication.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("users")
+public class UserController {
+	
+	@PostMapping
+	public String createUser() {
+		return "user created";
+		
+	}
+	
+	@GetMapping(value = "/getUser")
+	public String getUser() {
+		return "user retrieved";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "user updated";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "user deleted";
+	}
+	
 }
